10e64809ff41a442ec1d6e78f85c34683ab9e317
ecorth <ecorth378@gmail.com>
2015-05-04 22:16:48 -0500

    Funcion onclick y keyCode

--- git/tr01/js/app.js
+++ git/tr01/js/app.js
@@ -3,16 +3,42 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
- var main = function (){
-     "use strict";
-     $(".comment-input button").on("click", function(){
-         if((($(".comment-input input").val()) !== "")&&(($(".comment-input input").val()) !== " ")){
-         var $new_comment = $("<p>").text($(".comment-input input").val());
-         $(".comments").append($new_comment);
-         }
-     });
+/*var main = function (){
+ "use strict";
+ $(".comment-input button").on("click", function(){
+ if((($(".comment-input input").val()) !== "")&&(($(".comment-input input").val()) !== " ")){
+ var $new_comment = $("<p>").text($(".comment-input input").val());
+ $(".comments").append($new_comment);
+ }
+ });
  };
  $(document).ready(main);
- 
- //tomar el valor de entrada
+ */
+
+
+var main = function () {
+    "use strict";
+
+    var addCommentFromInputBox = function () {
+    var $new_comment;
+        if ($(".comment-input input").val() !== "") {
+            $new_comment = $("<p>").text($(".comment-input input").val());
+            $new_comment.hide();
+            $(".comments").append($new_comment);
+            $new_comment.fadeIn();
+            $(".comment-input input").val("");
+        }
+    };
+    $(".comment-input button").on("click", function (event) {
+        addCommentFromInputBox();
+    });
+    $(".comment-input input").on("keypress", function (event) {
+        if(event.keyCode === 13){
+            addCommentFromInputBox();
+        }
+    });
+};
+
+$(document).ready(main);
+//tomar el valor de entrada
  
